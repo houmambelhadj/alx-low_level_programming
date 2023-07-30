d7698efee5eabf07026b2ee3b3601d5bca78003a
houmambelhadj <houmblhj@gmail.com>
2023-07-30 04:08:52 -0700

    yeeh

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+nclude"main.h"
+
+/**
+ * rev_string - chaeck code
+ * @s: str.
+ * Return: noo.
+ */
+
+void rev_string(char *s)
+{
+	int	len;
+	int	i;
+	int	tmp;
+
+	len = 0;
+	i = 0;
+	while (s[len])
+		len++;
+	len--;
+	while (i < len)
+	{
+		tmp = s[i];
+		s[i] = s[len];
+		s[len] = tmp;
+		len--;
+		i++;
+	}
+}
